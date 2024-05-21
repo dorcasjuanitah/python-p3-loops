522ac2ae56f9b0072e88f3ef5abab68cb0969050
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-05-21 15:55:10 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        return(i -= 1)
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_int_list=[int_list * 2 for int in int_list]
+    return (squared_int_list)
 
-def fizzbuzz():
-    # code goes here!
-    pass
+def fizzbuzz(num):
+    for num in range (100):
+        return(num)
+
+    if (num % 3):
+        return("Fizz")
+    elif(num % 5):
+        return("Buzz")
+    elif(num % 3 and num % 5):
+        return ("FizzBuzz")
+    else:
+        return(num)
